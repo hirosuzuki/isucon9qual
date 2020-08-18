39848c96ee73d8abee96de0e7a1307ab1296cfb8
Hiro Suzuki <hiro.suzuki.oct10@gmail.com>
2020-08-18 16:05:19 +0000

    create index on items by time and id

--- go/09_ex_schema.sql
+++ go/09_ex_schema.sql
@@ -4,4 +4,7 @@ CREATE VIEW exitems AS SELECT items.*, \
 shippings.transaction_evidence_id, transaction_evidences.status transaction_evidence_status, shippings.status shipping_status \
 FROM items \
 LEFT JOIN transaction_evidences on transaction_evidences.item_id = items.id \
-LEFT JOIN shippings on shippings.transaction_evidence_id = transaction_evidences.id
+LEFT JOIN shippings on shippings.transaction_evidence_id = transaction_evidences.id;
+
+
+CREATE INDEX items_created_at_id_index ON items (created_at, id desc);
